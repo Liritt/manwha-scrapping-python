70fc07f310641a4dcb20fa453f9ed255a591fab4
Lirit <gaetanl2004@gmail.com>
2023-02-26 23:12:22 +0100

    feature: Ajout de la fonction get_manwhas_url.
    Fonction permettant de récupérer tout les liens des manwhas sur chacune des pages demandées

--- index.py
+++ index.py
@@ -12,6 +12,24 @@ def get_all_pages(number=1):
         i = f"https://mangakakalot.com/manga_list?type=latest&category=all&state=all&page={page_number}"
         page_number += 1
         urls.append(i)
-    print(urls)
 
     return urls
+
+
+def get_manwhas_url(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    manwhas = soup.find_all('div', class_='list-truyen-item-wrap')
+
+    urls = []
+    for manwha in manwhas:
+        link = manwha.find('a', href=True, title=True).get('href')
+        urls.append(link)
+
+    return urls
+
+
+for page in get_all_pages(2):
+    time.sleep(3)
+    print(get_manwhas_url(page))
